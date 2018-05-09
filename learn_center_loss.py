cvjena/semantic-embeddings

commit 09c728ebdfc0415c6f108151901153ff36afc7cd

Parallel input batch generation

learn_center_loss.py
@@ -40,10 +40,9 @@ def center_loss_model(base_model, centroids):
     return keras.models.Model([input_, cls_input_], [prob, center_loss])
 
 
-def gen_inputs(gen, num_classes):
+def transform_inputs(X, y, num_classes):
     
-    for X, y in gen:
-        yield [X, y], [keras.utils.to_categorical(y, num_classes), np.zeros(len(X))]
+    return [X, y], [keras.utils.to_categorical(y, num_classes), np.zeros(len(X))]
 
 
 
@@ -134,15 +133,17 @@ if __name__ == '__main__':
                       loss_weights = { 'prob' : 1.0, 'center_loss' : args.center_loss_weight },
                       metrics = { 'prob' : 'accuracy' })
 
+    batch_transform_kwargs = { 'num_classes' : data_generator.num_classes }
+
     par_model.fit_generator(
-              gen_inputs(data_generator.flow_train(args.batch_size), data_generator.num_classes),
-              data_generator.num_train // args.batch_size,
-              validation_data = gen_inputs(data_generator.flow_test(args.val_batch_size), data_generator.num_classes),
-              validation_steps = data_generator.num_test // args.val_batch_size,
-              epochs = args.epochs if args.epochs else num_epochs, callbacks = callbacks, verbose = not args.no_progress)
+              data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
+              callbacks = callbacks, verbose = not args.no_progress,
+              max_queue_size = 100, workers = 8, use_multiprocessing = True)
 
     # Evaluate final performance
-    print(par_model.evaluate_generator(gen_inputs(data_generator.flow_test(args.val_batch_size), data_generator.num_classes), data_generator.num_test // args.val_batch_size))
+    print(par_model.evaluate_generator(data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs)))
 
     # Save model
     if args.model_dump:

learn_ensemble.py
@@ -32,10 +32,9 @@ def build_ensemble(architecture, num_classes, embeddings, losses):
     return keras.models.Model(input_, [prob] + outputs)
 
 
-def gen_inputs(gen, embeddings):
+def transform_inputs(X, y, embeddings):
     
-    for X, y in gen:
-        yield (X, [keras.utils.to_categorical(y, embeddings[0].shape[0]), keras.utils.to_categorical(y, embeddings[0].shape[0])] + [embed[y] for embed in embeddings])
+    return X, [keras.utils.to_categorical(y, embeddings[0].shape[0]), keras.utils.to_categorical(y, embeddings[0].shape[0])] + [embed[y] for embed in embeddings]
 
 
 
@@ -130,15 +129,17 @@ if __name__ == '__main__':
                       metrics = dict([('prob', 'accuracy'), ('cnn0', 'accuracy')] + [('cnn{}'.format(i), utils.nn_accuracy(embedding, dot_prod_sim = (loss == 'inv_corr'))) for i, (embedding, loss) in enumerate(zip(embeddings, losses), 1)])
     )
 
+    batch_transform_kwargs = { 'embeddings' : embeddings }
+
     par_model.fit_generator(
-              gen_inputs(data_generator.flow_train(args.batch_size), embeddings),
-              data_generator.num_train // args.batch_size,
-              validation_data = gen_inputs(data_generator.flow_test(args.val_batch_size), embeddings),
-              validation_steps = data_generator.num_test // args.val_batch_size,
-              epochs = args.epochs if args.epochs else num_epochs, callbacks = callbacks, verbose = not args.no_progress)
+              data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
+              callbacks = callbacks, verbose = not args.no_progress,
+              max_queue_size = 100, workers = 8, use_multiprocessing = True)
 
     # Evaluate final performance
-    print(par_model.evaluate_generator(gen_inputs(data_generator.flow_test(args.val_batch_size), embeddings), data_generator.num_test // args.val_batch_size))
+    print(par_model.evaluate_generator(data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs)))
 
     # Save model
     if args.model_dump:

learn_labelembedding.py
@@ -27,10 +27,10 @@ def labelembed_loss(out1, out2, tar, targets, tau = 2., alpha = 0.9, beta = 0.5,
     
     pred = K.argmax(out2, axis = -1)
     mask = K.stop_gradient(K.cast(K.equal(pred, K.cast(targets, 'int64')), K.floatx()))
-    L_o1_emb = -cross_entropy(out1, soft_tar)
+    L_o1_emb = -cross_entropy(out1, soft_tar)  # pylint: disable=invalid-unary-operand-type
     
     L_o2_y = K.sparse_categorical_crossentropy(output = out2_prob, target = targets)
-    L_emb_o2 = -cross_entropy(tar, tau2_prob) * mask * (K.cast(K.shape(mask)[0], K.floatx())/(K.sum(mask)+1e-8))
+    L_emb_o2 = -cross_entropy(tar, tau2_prob) * mask * (K.cast(K.shape(mask)[0], K.floatx())/(K.sum(mask)+1e-8))  # pylint: disable=invalid-unary-operand-type
     L_re = K.relu(K.sum(out2_prob * K.one_hot(K.cast(targets, 'int64'), num_classes), axis = -1) - alpha)
     
     return beta * L_o1_y + (1-beta) * L_o1_emb + L_o2_y + L_emb_o2 + L_re
@@ -55,10 +55,9 @@ def labelembed_model(base_model, num_classes, **kwargs):
     return keras.models.Model([input_, cls_input_], [embedding, out1, loss])
 
 
-def gen_inputs(gen, num_classes):
+def transform_inputs(X, y, num_classes):
     
-    for X, y in gen:
-        yield [X, y], { 'labelembed_loss' : np.zeros((len(X), 1)), 'prob' : keras.utils.to_categorical(y, num_classes) }
+    return [X, y], { 'labelembed_loss' : np.zeros((len(X), 1)), 'prob' : keras.utils.to_categorical(y, num_classes) }
 
 
 
@@ -138,15 +137,17 @@ if __name__ == '__main__':
                       loss = { 'labelembed_loss' : lambda y_true, y_pred: y_pred[:,0], 'embedding' : None, 'prob' : lambda y_true, y_pred: K.tf.zeros(K.shape(y_true)[:1], dtype=K.floatx()) },
                       metrics = { 'prob' : 'accuracy' })
 
+    batch_transform_kwargs = { 'num_classes' : data_generator.num_classes }
+
     par_model.fit_generator(
-              gen_inputs(data_generator.flow_train(args.batch_size), data_generator.num_classes),
-              data_generator.num_train // args.batch_size,
-              validation_data = gen_inputs(data_generator.flow_test(args.val_batch_size), data_generator.num_classes),
-              validation_steps = data_generator.num_test // args.val_batch_size,
-              epochs = args.epochs if args.epochs else num_epochs, callbacks = callbacks, verbose = not args.no_progress)
+              data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
+              callbacks = callbacks, verbose = not args.no_progress,
+              max_queue_size = 100, workers = 8, use_multiprocessing = True)
 
     # Evaluate final performance
-    print(par_model.evaluate_generator(gen_inputs(data_generator.flow_test(args.val_batch_size), data_generator.num_classes), data_generator.num_test // args.val_batch_size))
+    print(par_model.evaluate_generator(data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs)))
     try:
         scores = par_model.predict_generator(data_generator.flow_test(args.val_batch_size, False), data_generator.num_test // args.val_batch_size)[1]
         print(np.mean(np.argmax(scores, axis = -1) == data_generator.y_test))

datasets.py
@@ -5,6 +5,7 @@ import os
 import warnings
 
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, list_pictures
+from keras.utils import Sequence
 from keras import backend as K
 
 try:
@@ -41,6 +42,47 @@ def get_data_generator(dataset, data_root, classes = None):
         return NABGenerator(data_root, classes, 'images_cropped_sq', mean = [124.31297374, 127.28434878, 115.89100229], std = [42.16245978, 30.43073297, 25.00008084])
     else:
         raise ValueError('Unknown dataset: {}'.format(dataset))
+
+
+
+class DataSequence(Sequence):
+
+    def __init__(self, data_generator, ids, labels, batch_size = 32, shuffle = False, batch_transform = None, batch_transform_kwargs = {}, **kwargs):
+
+        super(DataSequence, self).__init__()
+        self.data_generator = data_generator
+        self.ids = ids
+        self.labels = np.asarray(labels)
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.batch_transform = batch_transform
+        self.batch_transform_kwargs = batch_transform_kwargs
+        self.kwargs = kwargs
+
+        self.ind = np.arange(len(self.ids))
+        self.on_epoch_end()
+
+
+    def __len__(self):
+
+        return int(np.ceil(len(self.ids) / self.batch_size))
+
+
+    def __getitem__(self, idx):
+        
+        batch_ind = self.ind[idx:idx+self.batch_size]
+        X = self.data_generator.compose_batch([self.ids[i] for i in batch_ind], **self.kwargs)
+        y = self.labels[batch_ind]
+        if self.batch_transform is not None:
+            return self.batch_transform(X, y, **self.batch_transform_kwargs)  # pylint: disable=not-callable
+        else:
+            return X, y
+
+
+    def on_epoch_end(self):
+
+        if self.shuffle:
+            np.random.shuffle(self.ind)
 
 
 
@@ -114,8 +156,37 @@ class CifarGenerator(object):
         image_generator = self.image_generator if augment else self.test_image_generator
         return image_generator.flow(self.X_test, self.y_test if include_labels else None,
                                     batch_size=batch_size, shuffle=shuffle)
+
+
+    def train_sequence(self, batch_size = 32, shuffle = True, augment = True, batch_transform = None, batch_transform_kwargs = {}):
+        
+        return DataSequence(self, np.arange(len(self.X_train)), self.y_train,
+                            train=True, augment=augment,
+                            batch_size=batch_size, shuffle=shuffle, batch_transform=batch_transform, batch_transform_kwargs=batch_transform_kwargs)
     
     
+    def test_sequence(self, batch_size = 32, shuffle = False, augment = False, batch_transform = None, batch_transform_kwargs = {}):
+        
+        return DataSequence(self, np.arange(len(self.X_test)), self.y_test,
+                            train=False, augment=augment,
+                            batch_size=batch_size, shuffle=shuffle, batch_transform=batch_transform, batch_transform_kwargs=batch_transform_kwargs)
+    
+
+    def compose_batch(self, indices, train, augment = False):
+
+        X = self.X_train if train else self.X_test
+        image_generator = self.image_generator if augment else self.test_image_generator
+
+        batch = np.zeros((len(indices),) + tuple(X.shape[1:]), dtype=K.floatx())
+        for i, j in enumerate(indices):
+            x = X[j]
+            x = image_generator.random_transform(x.astype(K.floatx()))
+            x = image_generator.standardize(x)
+            batch[i] = x
+        
+        return batch
+    
+
     @property
     def labels_train(self):
         
@@ -200,15 +271,26 @@ class FileDatasetGenerator(object):
                           batch_size=batch_size, shuffle=shuffle, target_size=target_size,
                           normalize=True, hflip=augment, vflip=False, randzoom=augment, cropsize=self.cropsize, randcrop=augment, randerase=augment)
     
-    
-    def _flow(self, filenames, labels = None, batch_size = 32, shuffle = False, cropsize = None, randcrop = False, data_format = None, **kwargs):
+
+    def train_sequence(self, batch_size = 32, shuffle = True, target_size = None, augment = True, batch_transform = None, batch_transform_kwargs = {}):
         
-        if data_format is None:
-            data_format = K.image_data_format()
-        if data_format == 'channels_first':
-            x_axis, y_axis = 2, 1
-        else:
-            x_axis, y_axis = 1, 0
+        return DataSequence(self, self.train_img_files, self._train_labels,
+                            batch_size=batch_size, shuffle=shuffle,
+                            target_size=target_size, normalize=True, hflip=augment, vflip=False,
+                            randzoom=augment, cropsize=self.cropsize, randcrop=augment, randerase=augment,
+                            batch_transform=batch_transform, batch_transform_kwargs=batch_transform_kwargs)
+    
+    
+    def test_sequence(self, batch_size = 32, shuffle = False, target_size = None, augment = False, batch_transform = None, batch_transform_kwargs = {}):
+        
+        return DataSequence(self, self.test_img_files, self._test_labels,
+                            batch_size=batch_size, shuffle=shuffle,
+                            target_size=target_size, normalize=True, hflip=augment, vflip=False,
+                            randzoom=augment, cropsize=self.cropsize, randcrop=augment, randerase=augment,
+                            batch_transform=batch_transform, batch_transform_kwargs=batch_transform_kwargs)
+    
+    
+    def _flow(self, filenames, labels = None, batch_size = 32, shuffle = False, **kwargs):
         
         ind = np.arange(len(filenames))
         if shuffle:
@@ -228,38 +310,50 @@ class FileDatasetGenerator(object):
             batch_ind = ind[offs:offs+batch_size]
             offs += batch_size
             
-            X = [self._load_and_transform(filenames[i], data_format=data_format, **kwargs) for i in batch_ind]
-            if cropsize is not None:
-                crop_width, crop_height = cropsize
-            else:
-                crop_height = int(np.median([img.shape[y_axis] for img in X]))
-                crop_width = int(np.median([img.shape[x_axis] for img in X]))
-            for i, img in enumerate(X):
-                y_pad = x_pad = 0
-                if img.shape[y_axis] > crop_height:
-                    y_offs = np.random.randint(img.shape[y_axis] - crop_height + 1) if randcrop else (img.shape[y_axis] - crop_height) // 2
-                    img = img[:,y_offs:y_offs+crop_height,:] if data_format == 'channels_first' else img[y_offs:y_offs+crop_height,:,:]
-                elif img.shape[y_axis] < crop_height:
-                    y_pad = np.random.randint(crop_height - img.shape[y_axis] + 1) if randcrop else (crop_height - img.shape[y_axis]) // 2
-                if img.shape[x_axis] > crop_width:
-                    x_offs = np.random.randint(img.shape[x_axis] - crop_width + 1) if randcrop else (img.shape[x_axis] - crop_width) // 2
-                    img = img[:,:,x_offs:x_offs+crop_width] if data_format == 'channels_first' else img[:,x_offs:x_offs+crop_width,:]
-                elif img.shape[x_axis] < crop_width:
-                    x_pad = np.random.randint(crop_width - img.shape[x_axis] + 1) if randcrop else (crop_width - img.shape[x_axis]) // 2
-                X[i] = np.pad(
-                    img,
-                    ((0,0), (y_pad, crop_height - img.shape[1] - y_pad), (x_pad, crop_width - img.shape[2] - x_pad)) if data_format == 'channels_first' else \
-                    ((y_pad, crop_height - img.shape[0] - y_pad), (x_pad, crop_width - img.shape[1] - x_pad), (0,0)),
-                    'reflect'
-                )
-            X = np.stack(X)
-            
+            X = self.compose_batch([filenames[i] for i in batch_ind], **kwargs)
+
             if labels is not None:
                 yield X, labels[batch_ind]
             else:
                 yield X
-    
-    
+
+
+    def compose_batch(self, filenames, cropsize = None, randcrop = False, data_format = None, **kwargs):
+
+        if data_format is None:
+            data_format = K.image_data_format()
+        if data_format == 'channels_first':
+            x_axis, y_axis = 2, 1
+        else:
+            x_axis, y_axis = 1, 0
+
+        X = [self._load_and_transform(fn, data_format=data_format, **kwargs) for fn in filenames]
+        if cropsize is not None:
+            crop_width, crop_height = cropsize
+        else:
+            crop_height = int(np.median([img.shape[y_axis] for img in X]))
+            crop_width = int(np.median([img.shape[x_axis] for img in X]))
+        for i, img in enumerate(X):
+            y_pad = x_pad = 0
+            if img.shape[y_axis] > crop_height:
+                y_offs = np.random.randint(img.shape[y_axis] - crop_height + 1) if randcrop else (img.shape[y_axis] - crop_height) // 2
+                img = img[:,y_offs:y_offs+crop_height,:] if data_format == 'channels_first' else img[y_offs:y_offs+crop_height,:,:]
+            elif img.shape[y_axis] < crop_height:
+                y_pad = np.random.randint(crop_height - img.shape[y_axis] + 1) if randcrop else (crop_height - img.shape[y_axis]) // 2
+            if img.shape[x_axis] > crop_width:
+                x_offs = np.random.randint(img.shape[x_axis] - crop_width + 1) if randcrop else (img.shape[x_axis] - crop_width) // 2
+                img = img[:,:,x_offs:x_offs+crop_width] if data_format == 'channels_first' else img[:,x_offs:x_offs+crop_width,:]
+            elif img.shape[x_axis] < crop_width:
+                x_pad = np.random.randint(crop_width - img.shape[x_axis] + 1) if randcrop else (crop_width - img.shape[x_axis]) // 2
+            X[i] = np.pad(
+                img,
+                ((0,0), (y_pad, crop_height - img.shape[1] - y_pad), (x_pad, crop_width - img.shape[2] - x_pad)) if data_format == 'channels_first' else \
+                ((y_pad, crop_height - img.shape[0] - y_pad), (x_pad, crop_width - img.shape[1] - x_pad), (0,0)),
+                'reflect'
+            )
+        return np.stack(X)
+
+
     def _load_and_transform(self, filename, target_size = None, normalize = True, hflip = False, vflip = False, randzoom = False, randerase = False, data_format = None):
         
         if data_format is None:
@@ -420,3 +514,21 @@ class NABGenerator(FileDatasetGenerator):
         return self._flow(self.test_img_files, self._test_labels if include_labels else None,
                           batch_size=batch_size, shuffle=shuffle, target_size=target_size,
                           normalize=True, hflip=augment, vflip=False, randzoom=augment, cropsize=self.cropsize, randcrop=augment, randerase=augment)
+
+
+    def train_sequence(self, batch_size = 32, shuffle = True, target_size = 256, augment = True, batch_transform = None, batch_transform_kwargs = {}):
+        
+        return DataSequence(self, self.train_img_files, self._train_labels,
+                            batch_size=batch_size, shuffle=shuffle,
+                            target_size=target_size, normalize=True, hflip=augment, vflip=False,
+                            randzoom=augment, cropsize=self.cropsize, randcrop=augment, randerase=augment,
+                            batch_transform=batch_transform, batch_transform_kwargs=batch_transform_kwargs)
+    
+    
+    def test_sequence(self, batch_size = 32, shuffle = False, target_size = 256, augment = False, batch_transform = None, batch_transform_kwargs = {}):
+        
+        return DataSequence(self, self.test_img_files, self._test_labels,
+                            batch_size=batch_size, shuffle=shuffle,
+                            target_size=target_size, normalize=True, hflip=augment, vflip=False,
+                            randzoom=augment, cropsize=self.cropsize, randcrop=augment, randerase=augment,
+                            batch_transform=batch_transform, batch_transform_kwargs=batch_transform_kwargs)

learn_classifier.py
@@ -13,10 +13,9 @@ from datasets import DATASETS, get_data_generator
 
 
 
-def gen_inputs(gen, num_classes):
+def transform_inputs(X, y, num_classes):
     
-    for X, y in gen:
-        yield X, keras.utils.to_categorical(y, num_classes)
+    return X, keras.utils.to_categorical(y, num_classes)
 
 
 
@@ -104,16 +103,17 @@ if __name__ == '__main__':
     par_model.compile(optimizer = keras.optimizers.SGD(lr=args.sgd_lr, decay=decay, momentum=0.9, clipnorm = args.clipgrad),
                       loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
+    batch_transform_kwargs = { 'num_classes' : data_generator.num_classes }
+
     par_model.fit_generator(
-              gen_inputs(data_generator.flow_train(args.batch_size), data_generator.num_classes),
-              data_generator.num_train // args.batch_size,
-              validation_data = gen_inputs(data_generator.flow_test(args.val_batch_size), data_generator.num_classes),
-              validation_steps = data_generator.num_test // args.val_batch_size,
+              data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
               epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
-              callbacks = callbacks, verbose = not args.no_progress)
+              callbacks = callbacks, verbose = not args.no_progress,
+              max_queue_size = 100, workers = 8, use_multiprocessing = True)
 
     # Evaluate final performance
-    print(par_model.evaluate_generator(gen_inputs(data_generator.flow_test(args.val_batch_size), data_generator.num_classes), data_generator.num_test // args.val_batch_size))
+    print(par_model.evaluate_generator(data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs)))
 
     # Save model
     if args.model_dump:

learn_devise.py
@@ -13,10 +13,9 @@ from datasets import DATASETS, get_data_generator
 
 
 
-def gen_inputs(gen, embedding):
+def transform_inputs(X, y, embedding):
     
-    for X, y in gen:
-        yield (X, embedding[y])
+    return X, embedding[y]
 
 
 
@@ -75,6 +74,7 @@ if __name__ == '__main__':
         model.summary()
 
     callbacks = []
+    batch_transform_kwargs = { 'embedding' : embedding }
 
     if args.init_weights and (args.init_epochs > 0):
         print('Pre-training linear transformation')
@@ -86,12 +86,11 @@ if __name__ == '__main__':
                       metrics = [utils.nn_accuracy(embedding, dot_prod_sim = True)])
 
         model.fit_generator(
-                  gen_inputs(data_generator.flow_train(args.batch_size), embedding),
-                  data_generator.num_train // args.batch_size,
-                  validation_data = gen_inputs(data_generator.flow_test(args.val_batch_size), embedding),
-                  validation_steps = data_generator.num_test // args.val_batch_size,
-                  epochs = args.init_epochs,
-                  callbacks = callbacks, verbose = not args.no_progress)
+              data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              epochs = args.init_epochs,
+              callbacks = callbacks, verbose = not args.no_progress,
+              max_queue_size = 100, workers = 8, use_multiprocessing = True)
         
         for layer in model.layers[:-1]:
             layer.trainable = True
@@ -114,15 +113,14 @@ if __name__ == '__main__':
                       metrics = [utils.nn_accuracy(embedding, dot_prod_sim = True)])
 
         model.fit_generator(
-                  gen_inputs(data_generator.flow_train(args.batch_size), embedding),
-                  data_generator.num_train // args.batch_size,
-                  validation_data = gen_inputs(data_generator.flow_test(args.val_batch_size), embedding),
-                  validation_steps = data_generator.num_test // args.val_batch_size,
-                  epochs = args.ft_epochs,
-                  callbacks = callbacks, verbose = not args.no_progress)
+              data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
+              epochs = args.ft_epochs,
+              callbacks = callbacks, verbose = not args.no_progress,
+              max_queue_size = 100, workers = 8, use_multiprocessing = True)
 
     # Evaluate final performance
-    print(model.evaluate_generator(gen_inputs(data_generator.flow_test(args.val_batch_size), embedding), data_generator.num_test // args.val_batch_size))
+    print(par_model.evaluate_generator(data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs)))
 
     # Save model
     if args.model_dump:

commit 1b257b2639865b06b71f597f244111abbb7238f7

Fixed minor syntactical bug introduced in 09c728ebdfc0415c6f108151901153ff36afc7cd

learn_center_loss.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
     par_model.fit_generator(
               data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
               validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
-              epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
+              epochs = args.epochs if args.epochs else num_epochs,
               callbacks = callbacks, verbose = not args.no_progress,
               max_queue_size = 100, workers = 8, use_multiprocessing = True)
 

learn_ensemble.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
     par_model.fit_generator(
               data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
               validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
-              epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
+              epochs = args.epochs if args.epochs else num_epochs,
               callbacks = callbacks, verbose = not args.no_progress,
               max_queue_size = 100, workers = 8, use_multiprocessing = True)
 

learn_labelembedding.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     par_model.fit_generator(
               data_generator.train_sequence(args.batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
               validation_data = data_generator.test_sequence(args.val_batch_size, batch_transform = transform_inputs, batch_transform_kwargs = batch_transform_kwargs),
-              epochs = args.epochs if args.epochs else num_epochs, initial_epoch = args.initial_epoch,
+              epochs = args.epochs if args.epochs else num_epochs,
               callbacks = callbacks, verbose = not args.no_progress,
               max_queue_size = 100, workers = 8, use_multiprocessing = True)
 