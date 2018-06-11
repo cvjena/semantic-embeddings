8ea25026658273a7e430e54cbe12f2bbd83dfb03
Björn Barz <bjoern.barz@uni-jena.de>
2018-06-11 09:52:38 +0200

    CLI arguments for saving weights only

--- learn_devise.py
+++ learn_devise.py
@@ -40,7 +40,8 @@ if __name__ == '__main__':
     arggroup.add_argument('--max_decay', type = float, default = 0.0, help = 'Learning Rate decay at the end of training.')
     arggroup.add_argument('--margin', type = float, default = 0.1, help = 'Margin of the hinge ranking loss.')
     arggroup = parser.add_argument_group('Output parameters')
-    arggroup.add_argument('--model_dump', type = str, default = None, help = 'Filename where the learned model should be written to.')
+    arggroup.add_argument('--model_dump', type = str, default = None, help = 'Filename where the learned model definition and weights should be written to.')
+    arggroup.add_argument('--weight_dump', type = str, default = None, help = 'Filename where the learned model weights should be written to (without model definition).')
     arggroup.add_argument('--feature_dump', type = str, default = None, help = 'Filename where learned embeddings for test images should be written to.')
     arggroup.add_argument('--log_dir', type = str, default = None, help = 'Tensorboard log directory.')
     arggroup.add_argument('--no_progress', action = 'store_true', default = False, help = 'Do not display training progress, but just the final performance.')
@@ -125,8 +126,16 @@ if __name__ == '__main__':
     print(model.evaluate_generator(gen_inputs(data_generator.flow_test(args.val_batch_size), embedding), data_generator.num_test // args.val_batch_size))
 
     # Save model
+    if args.weight_dump:
+        try:
+            model.save_weights(args.weight_dump)
+        except Exception as e:
+            print('An error occurred while saving the model weights: {}'.format(e))
     if args.model_dump:
-        model.save(args.model_dump)
+        try:
+            model.save(args.model_dump)
+        except Exception as e:
+            print('An error occurred while saving the model: {}'.format(e))
 
     # Save test image embeddings
     if args.feature_dump:
