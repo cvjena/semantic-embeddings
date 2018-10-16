386f866407f139ec0d914c193fdc8ffa4568114f
Björn Barz <bjoern.barz@uni-jena.de>
2018-10-16 13:28:52 +0200

    Fixed DenseNet compatibility with recent Keras version

--- models/DenseNet/densenet.py
+++ models/DenseNet/densenet.py
@@ -21,8 +21,10 @@ from keras.regularizers import l2
 from keras.utils.layer_utils import convert_all_kernels_in_model, convert_dense_weights_data_format
 from keras.utils.data_utils import get_file
 from keras.engine.topology import get_source_inputs
-from keras.applications.imagenet_utils import _obtain_input_shape
-from keras.applications.imagenet_utils import decode_predictions
+try:
+    from keras.applications.imagenet_utils import _obtain_input_shape, decode_predictions
+except ImportError:
+    from keras_applications.imagenet_utils import _obtain_input_shape, decode_predictions
 import keras.backend as K
 
 from subpixel import SubPixelUpscaling
