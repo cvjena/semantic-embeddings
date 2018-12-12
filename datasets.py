5b48171d27dfbae78df7570e084653b2d00d9799
Björn Barz <bjoern.barz@uni-jena.de>
2018-12-12 09:42:08 +0100

    Support for customizing file paths of NABGenerator

--- datasets.py
+++ datasets.py
@@ -919,7 +919,7 @@ class ILSVRCGenerator(FileDatasetGenerator):
 
 class NABGenerator(FileDatasetGenerator):
 
-    def __init__(self, root_dir, classes = None, img_dir = 'images',
+    def __init__(self, root_dir, classes = None, img_dir = 'images', img_list_file = 'images.txt', split_file = 'train_test_split.txt', label_file = 'image_class_labels.txt',
                  cropsize = (224, 224), default_target_size = 256, randzoom_range = None, randerase_prob = 0.5, randerase_params = { 'sl' : 0.02, 'sh' : 0.3, 'r1' : 0.3, 'r2' : 1./0.3 },
                  mean = [125.30513277, 129.66606421, 118.45121113], std = [57.0045467, 56.70059436, 68.44430446], color_mode = "rgb"):
         """ NABirds data generator.
@@ -932,6 +932,14 @@ class NABGenerator(FileDatasetGenerator):
                    If set to `None`, all available classes will be used and enumerated in ascending order.
         
         - img_dir: Name of the sub-directory of `root_dir` that contains the images in further sub-directories named by their class label.
+
+        - img_list_file: Name of a file (relative to `root_dir`) that contains tuples of image IDs and their filenames, separated by white-spaces, one tuple per line.
+
+        - split_file: Name of a file (relative to `root_dir`) that specifies the training/test split as tuples of image IDs and either "1" (for training)
+                      or "0" (for test) images.
+        
+        - label_file: Name of a file (relative to `root_dir`) that specifies the class labels for each image as tuples of image IDs and class labels, one per line,
+                      separated by white-spaces.
 
         - cropsize: Tuple with width and height of crops extracted from the images.
 
@@ -953,9 +961,9 @@ class NABGenerator(FileDatasetGenerator):
         super(NABGenerator, self).__init__(root_dir, classes = classes, cropsize = cropsize, default_target_size = default_target_size, randzoom_range = randzoom_range,
                                            randerase_prob = randerase_prob, randerase_params = randerase_params, color_mode = color_mode)
         self.imgs_dir = os.path.join(root_dir, img_dir)
-        self.img_list_file = os.path.join(root_dir, 'images.txt')
-        self.label_file = os.path.join(root_dir, 'image_class_labels.txt')
-        self.split_file = os.path.join(root_dir, 'train_test_split.txt')
+        self.img_list_file = os.path.join(root_dir, img_list_file)
+        self.label_file = os.path.join(root_dir, label_file)
+        self.split_file = os.path.join(root_dir, split_file)
         
         # Read train/test split information
         with open(self.split_file) as f:
@@ -972,7 +980,7 @@ class NABGenerator(FileDatasetGenerator):
             for l in f:
                 if l.strip() != '':
                     img_id, fn = l.strip().split()
-                    if img_labels[img_id] in self.class_indices:
+                    if (img_id in is_train) and (img_labels[img_id] in self.class_indices):
                         if is_train[img_id]:
                             self.train_img_files.append(os.path.join(self.imgs_dir, fn))
                             self._train_labels.append(self.class_indices[img_labels[img_id]])
